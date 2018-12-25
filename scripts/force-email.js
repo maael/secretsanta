1ab5c56a7bd9e844539118a52442cc95cd4c423f
Matthew Elphick <matthew.elphick@threadsstyling.com>
2018-12-25 18:41:08 +0000

    fix: 🐛 Improve force script

--- scripts/force-email.js
+++ scripts/force-email.js
@@ -1,25 +1,32 @@
 const { reveal } = require("../server/api/lib/agendaJobs");
 
+console.info("Loaded reveal", reveal && typeof reveal === "function");
+
 const secretId = process.argv.slice(-1).pop();
 
-if (secretId) {
-  console.info("Forcing email for", secretId);
-  await reveal(
-    {
-      attrs: {
-        data: {
-          secret: secretId,
+console.info("Received arg", secretId);
+
+(async () => {
+  console.info("Starting force");
+  if (secretId) {
+    console.info("Forcing email for", secretId);
+    await reveal(
+      {
+        attrs: {
+          data: {
+            secret: secretId,
+          },
         },
       },
-    },
-    err => {
-      if (err) {
-        console.error("Error forcing", secretId, ":", err);
-      } else {
-        console.info("Successfully forced", secretId);
-      }
-    },
-  );
-} else {
-  console.error("Skipping, no secret given");
-}
+      err => {
+        if (err) {
+          console.error("Error forcing", secretId, ":", err);
+        } else {
+          console.info("Successfully forced", secretId);
+        }
+      },
+    );
+  } else {
+    console.error("Skipping, no secret given");
+  }
+})();
